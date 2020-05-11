dfabf1e75bb1fc9f596179078e8136e724dd4e77
hayjmuir <haymuir@gmail.com>
2020-05-11 14:54:23 -0700

    formatted JS

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
 var express = require("express")
 var bodyParser = require("body-parser")
-var methodOverride =require("method-override")
+var methodOverride = require("method-override")
 
 var PORT = process.env.PORT || 8080
 
@@ -9,12 +8,16 @@ var app = express()
 
 app.use(express.static("public"))
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(methodOverride("_method"))
 
 
 var exphbs = require("express-handlebars")
-app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}))
 app.set("view engine", "handlebars")
 
 // Import routes and give the server access to them.
@@ -23,7 +26,7 @@ var routes = require("./controllers/controller.js")
 app.use("/", routes)
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT)
 })
